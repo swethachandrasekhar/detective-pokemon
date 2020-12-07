331797ff755ebd532e820da231bd12b7ef7072a8
Stephen Thomas <stevekwthomas@gmail.com>
2020-12-07 22:35:13 +0000

    adding data

---
+++ src/dataStructures.js
@@ -0,0 +1,85 @@
+export const cityCoordinates = [
+  {
+    "name":"London",
+    "poly":"51.47589,-0.15156:51.47589,-0.10156:51.52589,-0.10156:51.52589,-0.15156"
+  },
+  {
+    "name":"Birmingham",
+    "poly":"52.43391,-1.942301:52.43391,-1.842301:52.53391,-1.842301:52.53391,-1.942301"
+  },
+  {
+    "name":"Liverpool",
+    "poly":"53.35585,-3.035683:53.35585,-2.935683:53.45585,-2.935683:53.45585,-3.035683"
+  },
+  {
+    "name":"Nottingham",
+    "poly":"52.904,-1.290101:52.904,-1.190101:53.004,-1.190101:53.004,-1.290101"
+  },
+  {
+    "name":"Sheffield",
+    "poly":"53.34563,-1.689539:53.34563,-1.589539:53.44563,-1.589539:53.44563,-1.689539"
+  }
+]
+
+export const crimeCategories = {
+  "anti-social-behaviour" : { 
+    "successfulType": 14,
+    "typeName": "psychic",
+    "reason": "Psychic pokemon are able to read and calm minds!"
+  },
+  "bicycle-theft" : { 
+    "successfulType": 3,
+    "typeName": "flying",
+    "reason": "Flying pokemon are able to fly and track the thief!"
+  },
+  "burglary" : { 
+    "successfulType": 2,
+    "typeName": "fighting",
+    "reason": "Fighting pokemon are able to beat the burglar in close quarter combat!"
+  },
+  "criminal-damage-arson" : { 
+    "successfulType": 11,
+    "typeName": "water",
+    "reason": "Water pokemon can put out the fire!"
+  },
+  "drugs" : { 
+    "successfulType": 4,
+    "typeName": "poison",
+    "reason": "Poison pokemon can detect drugs and create antidotes!"
+  },
+  "possession-of-weapons" : { 
+    "successfulType": 8,
+    "typeName": "ghost",
+    "reason": "Ghost pokemon can't be hurt by corporeal weapons!"
+  },
+  "public-order" : { 
+    "successfulType": 12,
+    "typeName": "grass",
+    "reason": "Grass pokemon can put people to sleep!"
+  },
+  "robbery" : { 
+    "successfulType": 13,
+    "typeName": "electric",
+    "reason": "Electric pokemon can stun the assailant!"
+  },
+  "shoplifting" : { 
+    "successfulType": 15,
+    "typeName": "ice",
+    "reason": "Ice pokemon can freeze the shoplifter in place!"
+  },
+  "theft-from-the-person" : { 
+    "successfulType": 1,
+    "typeName": "normal",
+    "reason": "Normal pokemon can stop theft from the person!"
+  },
+  "vehicle-crime" : { 
+    "successfulType": 16,
+    "typeName": "dragon",
+    "reason": "Dragon pokemon can fly and stop a car!"
+  },
+  "violent-crime" : { 
+    "successfulType": 18,
+    "typeName": "fairy",
+    "reason": "Fairy pokemon can turn any cruel person into a nice one!"
+  }
+}
