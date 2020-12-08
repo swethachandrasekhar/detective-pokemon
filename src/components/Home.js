fc06a7e304d083fc36152bf1f370e9cdc7b95fcd
Swetha Chandrasekhar <swethac68@gmail.com>
2020-12-07 22:01:43 -0500

    added functionality to fetch pokemons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,93 +1,112 @@
 import { Component } from 'react';
 import axios from 'axios';
 import { cityCoordinates, crimeCategories} from '../dataStructures.js'
+import Pokemons from "./Pokemons.js";
 
 class Home extends Component {
-    constructor(){
-        super();
-        this.state = {
-            crimeCategories: [],
-            selectedCrime: '',
-            selectedLocation: ''
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      crimeCategories: [],
+      selectedCrime: "",
+      selectedLocation: "",
+     
+      successPokemonType: '',
+    };
+  }
 
-    componentDidMount(){
-        // store each properties in an array
-        const crimeArray = Object.keys(crimeCategories);
-        this.setState({
-            crimeCategories: crimeArray
-        })
-    }
+  componentDidMount() {
+    // store each properties in an array
+    const crimeArray = Object.keys(crimeCategories);
+    this.setState({
+      crimeCategories: crimeArray,
+    });
+  }
 
-    getCrime = (customArea, crimeCategory) => {
-        return axios({
-            url: `https://data.police.uk/api/crimes-street/${crimeCategory}`,
-            responseType: 'json',
-            method: 'GET',
-            params: {
-                poly: customArea
-            }
-        })
-    }
+  getCrime = (customArea, crimeCategory) => {
+    return axios({
+      url: `https://data.police.uk/api/crimes-street/${crimeCategory}`,
+      responseType: "json",
+      method: "GET",
+      params: {
+        poly: customArea,
+      },
+    });
+  };
 
-    onLocationChange = (e) => {
-        this.setState({
-            selectedLocation: e.target.value
-        })
+  getPokemonType = (userSelectedCrime) => {
+    let successType;
+    for (const key in crimeCategories) {
+      if (key === userSelectedCrime) {
+        successType = crimeCategories[key].successfulType;
+      }
     }
+    console.log(successType);
+    return successType;
+  };
+
+  onLocationChange = (e) => {
+    this.setState({
+      selectedLocation: e.target.value,
+    });
+  };
+
+  handleCrime = (e) => {
+    this.setState({
+      selectedCrime: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
     
+    const pokemonType = this.getPokemonType(this.state.selectedCrime);
+    this.setState({
+        successPokemonType: pokemonType
+    })
+  };
 
-    handleCrime = (e) => {
-        this.setState({
-            selectedCrime: e.target.value
-        })
-    }
+  render() {
+      console.log(this.state.successPokemonType);
+    return (
+      <>
+        <h1>Detective Pokémon</h1>
+        <form>
+          <select onChange={this.onLocationChange}>
+            {cityCoordinates.map((place) => {
+              return (
+                <option key={place.poly} value={place.poly}>
+                  {place.name}
+                </option>
+              );
+            })}
+          </select>
 
-    handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(this.state.selectedLocation, this.state.selectedCrime)
-        const crime = this.getCrime(this.state.selectedLocation, this.state.selectedCrime);
-        crime.then((res) => {
-            console.log(res)
-        })
+          <select onChange={this.handleCrime}>
+            {this.state.crimeCategories.map((category, index) => {
+              return (
+                <option key={index} value={category}>
+                  {category}
+                </option>
+              );
+            })}
+          </select>
 
-    }
-
-
-    render() {
-        return (
-            <>
-                <h1>Detective Pokémon</h1>
-
-                <form>
-                    <select onChange={this.onLocationChange}>
-                        {cityCoordinates.map((place ) => {
-                            return(
-                                <option key={place.poly} value={place.poly}>
-                                    {place.name}
-                                </option>
-                            )
-                        })}
-                    </select>
-
-                    <select onChange={this.handleCrime}>
-                        {
-                            this.state.crimeCategories.map((category, index) => {
-                                return(
-                                    <option key={index} value={category}>{
-                                        category
-                                    }</option>
-                                )
-                            })
-                        }
-                    </select>
-
-                    <button onClick={this.handleSubmit}>Submit</button>
-                </form>
-            </>
-        );
-    }
+          <button onClick={this.handleSubmit}>Submit</button>
+        </form>
+        <div>
+        {
+        (this.state.successPokemonType)
+        ? <Pokemons
+          crime={this.state.selectedCrime}
+          successPokemonType={this.state.successPokemonType}
+        />
+       : null }
+        </div>
+        
+      </>
+    );
+  }
 }
 
 export default Home;
---
+++ src/components/Pokemons.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import axios from "axios";
+
+class Pokemons extends Component {
+  constructor() {
+    super();
+    this.state = {
+      displayPokemonList: [],
+    };
+  }
+
+  componentDidMount() {
+      console.log(this.props.successPokemonType);
+
+   this.getPokemons(this.props.successPokemonType);
+    // console.log(newPokemonArray);
+    
+  }
+
+  getPokemons = async (successPokemonType) => {
+    //Make an API call to get a list of Pokemons for Successful Pokemon Type
+    //Store the top result in the displayPokemons List
+    let newPokemonArray = [];
+    console.log(`test`);
+    let successfulPokemon = await this.getPokemonsAPICall(successPokemonType);
+    //Push the success pokemon object along with a success flag
+    let pokemObject =
+      successfulPokemon.data.pokemon[this.getRandomIndex(10)].pokemon;
+    pokemObject.match = "success";
+    newPokemonArray.push(pokemObject);
+    console.log(newPokemonArray);
+    //Make an API call to get 4 random pokemon of different types
+    let unsuccessfulPokemonPromises = [];
+    for (let i = 0; i < 4; i++) {
+      unsuccessfulPokemonPromises[i] = await this.getPokemonsAPICall(
+        this.getRandomIndex(100)
+      );
+    }
+
+    //Parse this promise array to create a list of unsuccessful pokemons
+    const unsuccessfulArray = this.parseUnsuccessfulPokemonPromises(
+      unsuccessfulPokemonPromises
+    );
+
+    //Push the unsuccessful pokemons into the pokemons array
+    newPokemonArray.push(...unsuccessfulArray);
+    // newPokemonArray = Object.values(newPokemonArray)  
+    console.log(newPokemonArray);      
+    // Set the display list with the pokemon array
+    this.setState({
+      displayPokemonList: newPokemonArray,
+    });
+    // return newPokemonArray
+  };
+
+
+  getPokemonsAPICall = (PokemonType) => {
+    return axios({
+      method: "GET",
+      url: `https://pokeapi.co/api/v2/type/3`,
+      dataResponse: "json",
+    });
+  };
+
+  
+  
+  getRandomIndex = (limit) => {
+    //   get an index between 0 to 9
+
+    const index = Math.floor(Math.random() * limit);
+    return index;
+  };
+
+  
+  parseUnsuccessfulPokemonPromises = (PokemonPromises) => {
+    console.log(PokemonPromises);
+    let unSuccessfulPokemonArray = [];
+    for (let i = 0; i < PokemonPromises.length; i++) {
+      let pokemObject =
+        PokemonPromises[i].data.pokemon[this.getRandomIndex(100)].pokemon;
+      pokemObject.match = "fail";
+      console.log(pokemObject);
+      unSuccessfulPokemonArray.push(pokemObject);
+    }
+    return unSuccessfulPokemonArray;
+  };
+
+  render() {
+    return(
+
+        
+            this.state.displayPokemonList
+            ? 
+            this.state.displayPokemonList.map((pokemon) => {
+              return(<p>{pokemon.name}</p>)  
+            })
+            : null
+       
+    )
+  }
+}
+
+export default Pokemons;
